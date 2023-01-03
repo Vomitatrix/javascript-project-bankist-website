433975ada7c1eaa0ec25ac94afd9b64d2e87242a
xubuntu-laptop <david.j.procop@gmail.com>
2023-01-02 22:06:25 -0500

    Added cookie popup div at the start of the page

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const allSections = document.querySelectorAll('.section');
+const allButtons = document.getElementsByTagName('button');
+const header = document.querySelector('.header');
 
 function openModal(e) {
     e.preventDefault();
@@ -23,4 +26,13 @@ overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', e => {
     if (e.key === 'Escape' && !modal.classList.constains('hidden')) closeModal();
+});
+
+const cookieMessage = document.createElement('div');
+cookieMessage.classList.add('cookie-message');
+cookieMessage.innerHTML =
+    'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+header.prepend(cookieMessage);
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+    cookieMessage.remove();
 });
